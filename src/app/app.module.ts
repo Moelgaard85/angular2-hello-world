787ac5626f6720e5d5daea891662d411d5fce1f5
Bjarne <bgp@proreact.dk>
2017-03-21 12:27:37 +0100

    Creating ngFor with input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { UserItemComponent } from './user-item/user-item.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorldComponent,
+    UserItemComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/user-item/user-item.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-item',
+  templateUrl: './user-item.component.html',
+  styleUrls: ['./user-item.component.scss']
+})
+export class UserItemComponent implements OnInit {
+  name: string;
+
+  constructor() {
+    this.name = 'Felipe'; // set the name
+  }
+
+  ngOnInit() {
+  }
+
+}
